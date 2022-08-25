0472b831877e8bb17de630312603217d439604e8
xl <xl18845898715@gmail.com>
2022-08-25 09:52:25 +0800

    first

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,10 +1,12 @@
 // rem适配
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 3.75 + 'px';
 
+// 初始化 fastclick
 window.addEventListener('load', function () {
   FastClick.attach(document.body);
 }, false);
 
+// 多指错误禁止
 document.documentElement.addEventListener('touchmove', function (e) {
   if (e.touches.length > 1) {
   	e.preventDefault();
